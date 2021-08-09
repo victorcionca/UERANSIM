d692697963c48c0097d42240b32d762b38fdd549
Victor Cionca <victorcionca@gmail.com>
2021-08-09 20:50:28 +0100

    Looking at AMF selection

--- src/gnb/ngap/nnsf.cpp
+++ src/gnb/ngap/nnsf.cpp
@@ -13,10 +13,20 @@ namespace nr::gnb
 
 NgapAmfContext *NgapTask::selectAmf(int ueId)
 {
+    NgapAmfContext *retVal = NULL;
     // todo:
-    for (auto &amf : m_amfCtx)
-        return amf.second; // return the first one
-    return nullptr;
+    for (auto &amf : m_amfCtx){
+        m_logger->debug("AMF %s supports:", amf.second->amfName);
+        for (auto &plmn : amf.second->plmnSupportList){
+            m_logger->debug("%s", plmn->plmn);
+            for (auto &snssai : plmn->sliceSupportList.slices){
+                m_logger->debug("%d - %d", (uint8_t) snssai.sst, *(snssai.sd));
+            }
+        }
+        if (!retVal)
+            retVal = amf.second; // return the first one
+    }
+    return retVal;
 }
 
 NgapAmfContext *NgapTask::selectNewAmfForReAllocation(int ueId, int initiatedAmfId, int amfSetId)
